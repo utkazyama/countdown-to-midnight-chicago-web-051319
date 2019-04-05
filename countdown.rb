dbf6a670a698703844a21633c0b08bf9d15e3231
Yuta Katsuyama <ykatsuyama@id.iit.edu>
2019-04-05 06:42:22 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,18 +3,12 @@
 
 def countdown(number)
   while number>0
-  puts "#{number} SECOND(S)! "
+  puts "#{number} SECOND(S)!"
   number -=1
   end
   return "HAPPY NEW YEAR!"
 end
 
-  while seconds_to_midnight > 0
-    puts "#{seconds_to_midnight} SECOND(S)!"
-    seconds_to_midnight -= 1
-  end
-  "HAPPY NEW YEAR!"
-end
 
 def countdown_with_sleep(number)
    while number>0
@@ -25,14 +19,3 @@ def countdown_with_sleep(number)
   return "HAPPY NEW YEAR!"
 end
 
-def countdown(seconds_to_midnight)
-
-
-def countdown_with_sleep(seconds_to_midnight)
-  while seconds_to_midnight > 0
-    puts "#{seconds_to_midnight} SECOND(S)!"
-    sleep(1)
-    seconds_to_midnight -= 1
-  end
-  "HAPPY NEW YEAR!"
-end
